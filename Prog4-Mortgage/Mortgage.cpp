63e6686238b0918cd2fb259dc4601af5ebf0e0aa
Kaarlaax <47810959+Kaarlaax@users.noreply.github.com>
2019-03-10 08:44:49 -0500

    Update Mortgage.cpp

--- Prog4-Mortgage/Mortgage.cpp
+++ Prog4-Mortgage/Mortgage.cpp
@@ -1,1 +1,26 @@
 // implementation file for Mortgage class
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "Mortgage.h"
+using namespace std;
+
+int main()
+{
+  Mortgage mort;
+  
+  	//get data from user
+	mort.setLoan();
+	mort.setInterestRate();
+	mort.setYears();
+
+	//display output
+	cout << setprecision(2) << fixed;
+	cout << "Monthly payment you need to pay to the bank: $ \n";
+	cout << mort.getMonthlyPayment();
+
+	cout << "Total amount you paid to bank at the end of period was: $";
+	cout << mort.getTotalPaid();
+
+	return 0;
+}
